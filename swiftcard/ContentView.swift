531edee3b5145cc1239451260dc842854c3c03b6
totokori <totokori1@gmail.com>
2024-04-10 09:32:59 -0500

    Initial Commit

---
+++ swiftcard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swiftcard
+//
+//  Created by ROTSEVENKOV, MATVEY V. on 4/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftcard/swiftcardApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftcardApp.swift
+//  swiftcard
+//
+//  Created by ROTSEVENKOV, MATVEY V. on 4/10/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftcardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
